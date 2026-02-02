99178d2dd273423d308977468c6ebc45210f5e8c
Andriy <illiushkoandrey@gmail.com>
2026-02-02 17:55:05 +0200

    changed project to vite

---
+++ vite-project/src/main.js
@@ -0,0 +1,11 @@
+import './characters/characters.css';
+import './characters/characters.js';
+
+import './episodes/episodes.css';
+import './episodes/episodes.js';
+
+import './locations/locations.css';
+import './locations/locations.js';
+
+import './styles/base.css';
+import './styles/layout.css';
